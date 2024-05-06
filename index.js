f5988ce3feec33301598c764f2b2d9118587c041
Adi2895 <jangraaditya11@gmail.com>
2024-05-06 23:20:59 +0530

    hello_world

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use('/api/auth' , require('./routes/auth'))
 app.use('/api/notes' , require('./routes/notes'))
 
 app.get("/" , (req , res)=>{
-    res.send("Hello world"); 
+    res.send("Hello world_got working"); 
 })
 
 app.listen(port , ()=>{
